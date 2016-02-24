42bad29c13c19e524fbafdc0412d0043286da604
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-02-24 16:45:35 +0100

    Add a remover thing to the welcome message

--- js/cookie.js
+++ js/cookie.js
@@ -34,7 +34,7 @@ function elementFromHtml(html) {
 }
 
 function showWelcomeMessage() {
-    var welcomeMessage = elementFromHtml('<div class="first-visit-welcome"><div class="first-visit-welcome-image"></div><div class="first-visit-welcome-text"><p>Velkommen til Mærkelex!</p><p>Vi prøver at samle alle forløbs- og duelighedsmærker fra danske spejderkorps, inklusiv de hjemmelavede, som folk selv laver og sælger.</p><p>Hvis du finder ud af at vi mangler et mærke, eller der står noget på siden der er forkert, kan du <a href="mailto:kontakt@mærkelex.dk">sende os en email</a>.</p></div></div>');
+    var welcomeMessage = elementFromHtml('<div class="first-visit-welcome"><div class="first-visit-welcome-remover">Fjern besked</div><div class="first-visit-welcome-image"></div><div class="first-visit-welcome-text"><p>Velkommen til Mærkelex!</p><p>Vi prøver at samle alle forløbs- og duelighedsmærker fra danske spejderkorps, inklusiv de hjemmelavede, som folk selv laver og sælger.</p><p>Hvis du finder ud af at vi mangler et mærke, eller der står noget på siden der er forkert, kan du <a href="mailto:kontakt@mærkelex.dk">sende os en email</a>.</p></div></div>');
 
     var container = document.querySelector(".container");
     container.insertBefore(welcomeMessage, container.firstChild);
