53a316bbd2492fab225a6352e34f4081be810d9d
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-02-24 14:33:42 +0100

    Save a lookup of body (already exists on document)

--- js/cookie.js
+++ js/cookie.js
@@ -18,11 +18,11 @@ function userHasVisitedBefore() {
 }
 
 function showCookieNotice() {
-    var body = document.querySelector("body");
     var utilityDiv = document.createElement("div");
     utilityDiv.innerHTML = '<div class="cookieNotice">Vi bruger cookies for at forbedre bruger&#173;oplevelsen. Ved at bruge siden giver du dit samtykke til at vi samler anonym information om hvilke sider der besøges mest på siden.</div>';
     var cookieNotice = utilityDiv.firstChild;
-    body.appendChild(cookieNotice);
+
+    document.body.appendChild(cookieNotice);
     cookieNotice.addEventListener("click", function(event) {
         this.className += " hidden";
     });
