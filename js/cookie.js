54523a63dee38b52ff064fd79b3a8b45a10b3b2a
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-02-24 16:38:48 +0100

    Add welcome message on first visit only, like the cookie.

--- js/cookie.js
+++ js/cookie.js
@@ -1,6 +1,7 @@
 (function showCookieNoticeIfFirstVisit() {
     if(!userHasVisitedBefore()) {
         showCookieNotice();
+        showWelcomeMessage();
         setUserHasVisitedBefore();
     }
 })();
@@ -30,6 +31,13 @@ function elementFromHtml(html) {
     var utilityDiv = document.createElement("div");
     utilityDiv.innerHTML = html;
     return utilityDiv.firstChild;
+}
+
+function showWelcomeMessage() {
+    var welcomeMessage = elementFromHtml('<div class="first-visit-welcome"><div class="first-visit-welcome-image"></div><div class="first-visit-welcome-text"><p>Velkommen til Mærkelex!</p><p>Vi prøver at samle alle forløbs- og duelighedsmærker fra danske spejderkorps, inklusiv de hjemmelavede, som folk selv laver og sælger.</p><p>Hvis du finder ud af at vi mangler et mærke, eller der står noget på siden der er forkert, kan du <a href="mailto:kontakt@mærkelex.dk">sende os en email</a>.</p></div></div>');
+
+    var container = document.querySelector(".container");
+    container.insertBefore(welcomeMessage, container.firstChild);
 }
 
 function setUserHasVisitedBefore() {
