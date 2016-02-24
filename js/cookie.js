781aea78dca4ac3b63b1c71600f74a74683d4f25
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-02-24 14:31:38 +0100

    Do some js magic to make the cookie notice appear without rewriting innerHTML of body

--- js/cookie.js
+++ js/cookie.js
@@ -19,7 +19,10 @@ function userHasVisitedBefore() {
 
 function showCookieNotice() {
     var body = document.querySelector("body");
-    body.innerHTML += '<div class="cookieNotice">Vi bruger cookies for at forbedre bruger&#173;oplevelsen. Ved at bruge siden giver du dit samtykke til at vi samler anonym information om hvilke sider der besøges mest på siden.</div>';
+    var utilityDiv = document.createElement("div");
+    utilityDiv.innerHTML = '<div class="cookieNotice">Vi bruger cookies for at forbedre bruger&#173;oplevelsen. Ved at bruge siden giver du dit samtykke til at vi samler anonym information om hvilke sider der besøges mest på siden.</div>';
+    var cookieNotice = utilityDiv.firstChild;
+    body.appendChild(cookieNotice);
     document.querySelector(".cookieNotice").addEventListener("click", function(event) {
         this.className += " hidden";
     });
