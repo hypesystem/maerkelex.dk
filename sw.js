629b9d934323faf6e71cab0e035a36069239818b
Niels Abildgaard <niels.abildgaard@gmail.com>
2017-08-17 17:13:35 +0200

    sw responds from cache when available

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 ---
 ---
 self.addEventListener("install", function(e) {
-    debugger;
     e.waitUntil(
         caches
             .open("maerkelex-cache-v1.0.0")
             .then(function(cache) {
                 return cache.addAll([
                     "{{ site.baseUrl }}/",
+                    "{{ site.baseUrl }}/index.html",
                     "{{ site.baseUrl }}/css/style.css",
                     "{{ site.baseUrl }}/js/raw-search.js",
                     "{{ site.baseUrl }}/js/cookie.js",
@@ -17,6 +17,16 @@ self.addEventListener("install", function(e) {
                     {% endfor %}
                     "{{ site.baseUrl }}/m.summary.json"
                 ]);
+            })
+    );
+});
+
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches
+            .match(e.request)
+            .then(function(response) {
+                return response || fetch(e.request);
             })
     );
 });
