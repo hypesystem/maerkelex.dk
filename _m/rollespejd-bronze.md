65b07cc6326e5108f2d3174a349c9a4f69c0c7fb
maikenlindner <maiken@deranged.dk>
2024-08-30 13:43:41 +0200

    added quotation marks to all includes

--- _m/rollespejd-bronze.md
+++ _m/rollespejd-bronze.md
@@ -8,4 +8,4 @@ image: rollespejd-bronze.jpg
 price: 20
 date_added: 2019-04-02
 ---
-{% include rollespejd-decription.md %}
+{% include 'rollespejd-decription.md' %}
--- js/raw-search.js
+++ js/raw-search.js
@@ -136,7 +136,7 @@ function renderResults(matches) {
         {% assign m_highlight_class = "' + (match.highlight || '') + '" %}
         {% assign m_highlight_text = "' + (match.price ? (match.preorder ? '<strong>Forudbestil</strong>' : '<strong>Køb for ' + match.price + ' kr</strong>') : match.highlight == 'popular' ? 'Mest populære!' : match.highlight == 'new' ? 'Nyhed!' : 'Læs mere') + '" %}
         {% assign m_purchasable = "' + (match.price ? 'purchasable' : '') + '" %}
-        {% capture maerke_result %}'{% include maerkebox.html %}';{% endcapture %}
+        {% capture maerke_result %}'{% include 'maerkebox.html' %}';{% endcapture %}
         return {{ maerke_result | strip_newlines }}
     }).join("");
 }
