44da3395e75bf8f88f3d2e870a326632d124f324
maikenlindner <maiken@deranged.dk>
2024-09-10 15:25:07 +0200

    port to eleventy WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,33 @@
+const sass = require("sass");
+const path = require("node:path");
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("img");
+	eleventyConfig.addPassthroughCopy("css");
+
+	eleventyConfig.addTemplateFormats("scss");
+
+	// Creates the extension for use
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css", // optional, default: "html"
+
+		// `compile` is called once per .scss file in the input directory
+		compile: async function (inputContent, inputPath) {
+			let parsed = path.parse(inputPath);
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+				  parsed.dir || ".",
+				  "./_sass",
+				],
+			  });
+
+			// This is the render function, `data` is the full data cascade
+			return async (data) => {
+				return result.css;
+			};
+		},
+	});
+
 	return {
 		dir: {
 			includes: "_includes",
